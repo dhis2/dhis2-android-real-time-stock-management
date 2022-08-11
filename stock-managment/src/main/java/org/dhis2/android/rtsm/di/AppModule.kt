87ea21be69ecaf9f21fde188273fec9dd193b2c5
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2022-08-11 16:35:27 +0200

    coping di classes

---
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/di/AppModule.kt
@@ -0,0 +1,89 @@
+package org.dhis2.android.rtsm.di
+
+import android.content.Context
+import androidx.work.WorkManager
+import com.baosystems.icrc.psm.data.AppConfig
+import com.baosystems.icrc.psm.services.*
+import com.baosystems.icrc.psm.services.preferences.PreferenceProvider
+import com.baosystems.icrc.psm.services.preferences.PreferenceProviderImpl
+import com.baosystems.icrc.psm.services.rules.ExpressionEvaluatorImpl
+import com.baosystems.icrc.psm.utils.ConfigUtils
+import com.baosystems.icrc.psm.utils.Sdk
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import org.apache.commons.jexl2.JexlEngine
+import org.hisp.dhis.android.core.D2
+import org.hisp.dhis.rules.RuleExpressionEvaluator
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+    @Provides
+    @Singleton
+    fun providesAppConfig(@ApplicationContext appContext: Context): AppConfig {
+        return ConfigUtils.getAppConfig(appContext.resources)
+    }
+
+    @Provides
+    @Singleton
+    fun providesD2(@ApplicationContext appContext: Context): D2 {
+        return Sdk.d2(appContext)
+    }
+
+    @Provides
+    @Singleton
+    fun providesPreferenceProvider(@ApplicationContext appContext: Context): PreferenceProvider {
+        return PreferenceProviderImpl(appContext)
+    }
+
+    @Provides
+    @Singleton
+    fun providesWorkManager(@ApplicationContext appContext: Context): WorkManager {
+        return WorkManager.getInstance(appContext)
+    }
+
+    @Provides
+    @Singleton
+    fun providesWorkManagerController(workManager: WorkManager): WorkManagerController {
+        return WorkManagerControllerImpl(workManager)
+    }
+
+    @Provides
+    @Singleton
+    fun providesSyncManager(
+        d2: D2,
+        preferenceProvider: PreferenceProvider,
+        workManagerController: WorkManagerController
+    ): SyncManager {
+        return SyncManagerImpl(d2, preferenceProvider, workManagerController)
+    }
+
+    @Provides
+    @Singleton
+    fun providesJexlEngine(): JexlEngine {
+        return JexlEngine()
+    }
+
+    @Provides
+    @Singleton
+    fun providesRuleExpressionEvaluator(jexlEngine: JexlEngine): RuleExpressionEvaluator {
+        return ExpressionEvaluatorImpl(jexlEngine)
+    }
+
+    @Provides
+    @Singleton
+    fun providesSpeechRecognitionManager(@ApplicationContext appContext: Context):
+            SpeechRecognitionManager {
+        return SpeechRecognitionManagerImpl(appContext)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOpenIdProvider(@ApplicationContext appContext: Context): OpenIdProvider {
+        return OpenIdProviderImpl(appContext.applicationContext)
+    }
+}
---
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/di/ServicesModule.kt
@@ -0,0 +1,32 @@
+package org.dhis2.android.rtsm.di
+
+import com.baosystems.icrc.psm.services.*
+import com.baosystems.icrc.psm.services.rules.RuleValidationHelper
+import com.baosystems.icrc.psm.services.rules.RuleValidationHelperImpl
+import com.baosystems.icrc.psm.services.scheduler.BaseSchedulerProvider
+import com.baosystems.icrc.psm.services.scheduler.SchedulerProviderImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class ServicesModule {
+    @Binds
+    abstract fun providesSchedulerProvider(impl: SchedulerProviderImpl): BaseSchedulerProvider
+
+    @Binds
+    abstract fun provideMetadataManager(impl: MetadataManagerImpl): MetadataManager
+
+    @Binds
+    abstract fun provideUserManager(impl: UserManagerImpl): UserManager
+
+    @Binds
+    abstract fun provideStockManager(impl: StockManagerImpl): StockManager
+
+    @Binds
+    abstract fun provideProgramRuleValidationHelper(
+        impl: RuleValidationHelperImpl
+    ): RuleValidationHelper
+}
